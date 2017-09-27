889e4211106b63d14656858b06d63904f01c9c9c
twin10240 <twin10240@naver.com>
2017-09-27 17:29:17 +0900

    Initial commit

---
+++ bool.py
@@ -0,0 +1,20 @@
+# 참이나 거짓을 나타내는 True, False 두 상수를 갖는다
+
+a = 1
+b = a < 10
+
+print(b, type(b), sep=",")
+
+b1 = True
+b2 = False
+
+print(b1 + 10)
+print(b2 + 10)
+print(True + True)
+
+if a < 10:
+    pass
+
+if a < 10:
+    print(a)
+    
